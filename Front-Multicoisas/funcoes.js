84e9a897b2cf5c46b5e8cd7a20434e9cf0b75bfe
Jhonathan Luiz <jhonathangluiz@gmail.com>
2020-09-22 18:49:38 -0400

    Criacao do arquivo js para chamada das telas

---
+++ Front-Multicoisas/funcoes.js
@@ -0,0 +1,18 @@
+var registro={};
+var nome;
+
+function Entrar() {
+    window.open("HomeUsuario.html");
+}
+
+function Cadastrar(){
+    window.open("cadastro.html");
+}
+
+function ListaCadastrados() {
+    window.open("ListaCadastrados.html");
+}
+
+function Dados(nome){
+    registro = nome;
+}
